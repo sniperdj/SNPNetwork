63bdad52f9483cb615d0442108b0dd5466710a51
Sniper <sniper_zheng@163.com>
2019-01-02 22:57:46 +0800

    create a new project

---
+++ SNPNetwork/SNPNetwork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SNPNetwork
+//
+//  Created by Sniper on 2019/1/2.
+//  Copyright © 2019 Sniper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SNPNetwork/SNPNetworkTests/SNPNetworkTests.m
@@ -0,0 +1,37 @@
+//
+//  SNPNetworkTests.m
+//  SNPNetworkTests
+//
+//  Created by Sniper on 2019/1/2.
+//  Copyright © 2019 Sniper. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SNPNetworkTests : XCTestCase
+
+@end
+
+@implementation SNPNetworkTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
